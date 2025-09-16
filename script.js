579d06fbf95dd45bd80ffc239b7709e3a732b15b
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-09-16 16:52:37 +0300

    add feature

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let moment = require("moment")()
 function getDate(){
     return moment.format("y/D/M H:m:s")
 }
-function getCurrentDay(){
-    console.log(`current day is ${ moment.format("dddd")}`)
+function getCurrentDay(isString = true){
+    /* 
+    21313
+    */
+    let day = isString ? moment.format("dddd") : moment.format("d")
+    console.log(`current day is ${ day }`)
+    
 }
 function getCurrentMouth(){
     console.log(`current mouth is ${moment.format("MMMM")}`)
