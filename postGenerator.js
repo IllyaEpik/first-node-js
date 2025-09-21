f7e670a30aea717279cfd2dbf26270daa4b718e7
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-09-21 11:44:11 +0300

    ok

---
+++ postGenerator.js
@@ -0,0 +1,30 @@
+let path = require("path")
+let fs = require("fs")
+const crypto  = require("crypto")
+const randomWords = require('random-words')
+let imgList = [
+    "https://www.timeforkids.com/wp-content/uploads/2019/09/final-cover-forest.jpg?w=1024",
+    "https://upload.wikimedia.org/wikipedia/ru/f/f4/Minecraft_Cover_Art.png",
+    "https://www.exitlag.com/blog/wp-content/uploads/2024/12/How-to-build-a-cute-Minecraft-house_-materials-design-and-tips-to-create-your-world.webp",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaNYcBxd8JRW3HuLY5Gwoq0yRSZYmOxlH3Fw&s",
+    ""
+]
+function createPost(countPosts=10){
+    array = []
+    for (let count = 0; count<countPosts; count++){
+        let object = {
+            id: array.length+1,
+            name: randomWords.generate(crypto.randomInt(2)+1).join(" "),
+            description: randomWords.generate(crypto.randomInt(200)+10).join(" "),
+            img: imgList[crypto.randomInt(5)],
+            likes:crypto.randomInt(10000)
+        }
+        array.push(object)
+    }
+    fs.writeFileSync(
+        path.join(__dirname+"/posts.json"),
+        JSON.stringify(array,null,4)
+    )
+    return array
+}
+module.exports = createPost
--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 let moment = require("moment")()
-const express = require("express")()
+let express = require("express")
+let app = express()
+app.use(require("express").json());
+app.use(express.urlencoded({ extended: true }));
+
+let path = require("path")
+let fs = require("fs")
+createPost = require("./postGenerator.js")
+
 const HOST = "127.0.0.1"
 const PORT = 8888
-express.get("/timestamp", (req,res) => {
+let pathToJson = path.join(__dirname+"/posts.json")
+app.get("/timestamp", (req,res) => {
     res.status(200).send({date: moment.format("y/D/M H:m:s")})
 })
-express.listen(PORT,HOST, () =>{ 
+app.get("/posts", (req,res) => {
+    let data = JSON.parse(fs.readFileSync(pathToJson,'utf-8'))
+    res.status(200).json(data)
+})
+app.post("/posts", (req,res) => {
+    if (req.body){
+        if ("count" in req.body){
+            if (!isNaN(Number(req.body.count))){
+                createPost(req.body["count"])
+                // res.status(200).send("update complete")
+                let data = JSON.parse(fs.readFileSync(pathToJson,'utf-8'))
+                res.status(200).json(data)
+            }else{
+                res.status(400).send("count must be number")
+            }
+        }else{
+            res.status(400).send("body must have count of posts")
+        }
+    }else{
+        res.status(400).send("request doesn't have body or server can't get body, try to set type of body 'raw'")
+    }
+    
+})
+app.listen(PORT,HOST, () =>{ 
     console.log(`http://${HOST}:${PORT}`)
 })
