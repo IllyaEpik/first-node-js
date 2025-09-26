afa3f3c394425dfee7fabee14deb8f5e6ff9a494
Illya <121565465+IllyaEpik@users.noreply.github.com>
2025-09-27 00:28:39 +0300

    query: take, skip, filter

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let moment = require("moment")()
 let express = require("express")
 let app = express()
-app.use(require("express").json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 let path = require("path")
 let fs = require("fs")
@@ -16,6 +16,34 @@ app.get("/timestamp", (req,res) => {
 })
 app.get("/posts", (req,res) => {
     let data = JSON.parse(fs.readFileSync(pathToJson,'utf-8'))
+    let skip = 0
+    let take = 999999
+    let filter = !!req.query.filter
+    if (!isNaN(Number(req.query.skip))){
+        skip = Number(req.query.skip)
+    }else if (req.query.skip){
+        res.status(400).json("skip must be a number")
+    }
+    if (!isNaN(Number(req.query.take))){
+        take = Number(req.query.take)
+    }else if (req.query.take){
+        res.status(400).json("take must be a number")
+    }
+    
+    data = data.filter((item) => {
+        if (skip>0){
+            skip-- 
+            return false;
+        }
+        if (take>0){
+            if (filter){
+                take-=item.name.includes("a")
+                return item.name.includes("a")
+            }
+            take--
+            return true;
+        }
+    })
     res.status(200).json(data)
 })
 app.post("/posts", (req,res) => {
@@ -23,7 +51,6 @@ app.post("/posts", (req,res) => {
         if ("count" in req.body){
             if (!isNaN(Number(req.body.count))){
                 createPost(req.body["count"])
-                // res.status(200).send("update complete")
                 let data = JSON.parse(fs.readFileSync(pathToJson,'utf-8'))
                 res.status(200).json(data)
             }else{
@@ -36,6 +63,21 @@ app.post("/posts", (req,res) => {
         res.status(400).send("request doesn't have body or server can't get body, try to set type of body 'raw'")
     }
     
+})
+app.get("/posts/:id", (req,res) => {
+    const id = req.params.id
+    let data = JSON.parse(fs.readFileSync(pathToJson,'utf-8'))
+    for (let object of data){
+        
+        if (object.id == id) {
+            res.status(200).json(object)
+
+            return;
+        }
+        
+    }
+    console.log(123)
+    res.status(400).send(`doesn't exists post with id ${id}`)
 })
 app.listen(PORT,HOST, () =>{ 
     console.log(`http://${HOST}:${PORT}`)
